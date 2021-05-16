e13b007cafa091ca71c2e6b2d581ec21c672cad2
Kellan2002 <45543616+Kellan2002@users.noreply.github.com>
2021-05-16 20:58:22 +0200

    Disable OneDrive (Optional), Disable Cortana

--- script.ps1
+++ script.ps1
@@ -7,6 +7,12 @@ Write-Host "Creating Restore Point incase something bad happens"
 Enable-ComputerRestore -Drive "C:\" # Sets restore point to C drive
 Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
 
+Write-Host "Running O&O Shutup with Recommended Settings"
+Import-Module BitsTransfer		choco install shutup10 -y
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/ooshutup10.cfg" -Destination ooshutup10.cfg        OOSU10 ooshutup10.cfg /quiet
+Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe
+./OOSU10.exe ooshutup10.cfg /quiet
+
 Write-Host "Disabling Telemetry..."
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
@@ -289,6 +295,63 @@ foreach ($Paint3D in $Paint3Dstuff) {
   Set-Item $Paint3D $rmPaint3D
 }
 }
+
+Write-Host "Disabling Cortana..."
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -Type DWord -Value 0
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitTextCollection" -Type DWord -Value 1
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Type DWord -Value 1
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Type DWord -Value 0
+    Write-Host "Disabled Cortana"
+
+Write-Host "Enabling Dark Mode"
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+Write-Host "Enabled Dark Mode"
+    
+}
+
+$OneDrive = Read-Host "Do you want to remove OneDrive? Yes[y]"
+
+if ($OneDrive -eq "y" -or $OneDrive -eq "Y")
+{
+  Write-Host "Disabling OneDrive..."
+  If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive")) {
+      New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" | Out-Null
+  }
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Type DWord -Value 1
+  Write-Host "Uninstalling OneDrive..."
+  Stop-Process -Name "OneDrive" -ErrorAction SilentlyContinue
+  Start-Sleep -s 2
+  $onedrive = "$env:SYSTEMROOT\SysWOW64\OneDriveSetup.exe"
+  If (!(Test-Path $onedrive)) {
+      $onedrive = "$env:SYSTEMROOT\System32\OneDriveSetup.exe"
+  }
+  Start-Process $onedrive "/uninstall" -NoNewWindow -Wait
+  Start-Sleep -s 2
+  Stop-Process -Name "explorer" -ErrorAction SilentlyContinue
+  Start-Sleep -s 2
+  Remove-Item -Path "$env:USERPROFILE\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+  Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+  Remove-Item -Path "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+  Remove-Item -Path "$env:SYSTEMDRIVE\OneDriveTemp" -Force -Recurse -ErrorAction SilentlyContinue
+  If (!(Test-Path "HKCR:")) {
+      New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+  }
+  Remove-Item -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -ErrorAction SilentlyContinue
+  Remove-Item -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -ErrorAction SilentlyContinue
+  Write-Host "Disabled OneDrive"
 }
 
 
@@ -330,6 +393,9 @@ if ($ChocoChoice -eq "f")
 
     Write-Host "Installing Steam"
     choco install steam-client -y
+
+    Write-Host "Installing Epic Games Launcher"
+    choco install epicgameslauncher -y
 }
 
 elseif ($ChocoChoice -eq "m")
