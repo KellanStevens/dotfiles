4a7a24d3d6d84d6d581a58b802c8d52a6923adc0
Kellan2002 <45543616+Kellan2002@users.noreply.github.com>
2021-05-24 10:14:34 +0200

    using case statements instead of if statements for cleaner code

--- script.sh
+++ script.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+# 1.1 Input is currentState($1) and totalState($2)
+function ProgressBar {
+# Process data
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+# Build progressbar string lengths
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:                           
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+
+}
+
+_command(){
+  case $i in
+  1) defaults write com.apple.dock "orientation" -string "left" && killall Dock;;
+  2) defaults write com.apple.dock "autohide" -bool "true" && killall Dock;;
+  3) defaults write com.apple.dock autohide-time-modifier -float 0.125 && killall Dock;;
+  4) defaults write com.apple.dock "show-recents" -bool "false" && killall Dock;;
+  5) defaults write com.apple.screencapture type JPG;;
+  6) defaults write com.apple.screencapture "disable-shadow" -bool "true" ;;
+  7) defaults write com.apple.screencapture "location" -string "~/Pictures/Screenshots" && killall SystemUIServer;;
+  8) defaults write com.apple.Finder "AppleShowAllFiles" -bool "true" && killall Finder;;
+  9) defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "true" && killall Music;;
+  esac
+}
+_command
+
+# Variables
+_start=1
+
+# This accounts as the "totalState" variable for the ProgressBar function
+_end=100
 
 echo "Hello welcome to Kellan's bash script for bootstrapping MacOS"
 #read -n1 -p "Have you installed Apple's command line tools (xcode-select)? Yes[y]/No[n] " xcode_installed
@@ -43,4 +80,53 @@ done
 echo
 echo $flag
 
-if $flag 
+i=1
+
+for i in {1..9}; do
+  _command $i
+done
+
+# if $flag -eq true; then
+# # echo -ne '#####                     (33%)\r'
+# # sleep 1
+# # echo -ne '#############             (66%)\r'
+# # sleep 1
+# # echo -ne '#######################   (100%)\r'
+# # echo -ne '\n'
+  
+#   echo "Moving dock to left"
+#   defaults write com.apple.dock "orientation" -string "left" && killall Dock
+  
+#   echo "Enabling dock autohide"
+#   defaults write com.apple.dock "autohide" -bool "true" && killall Dock
+  
+#   echo "Increasing dock unhide animation"
+#   defaults write com.apple.dock autohide-time-modifier -float 0.125 && killall Dock
+  
+#   echo "Disabling recent apps in dock"
+#   defaults write com.apple.dock "show-recents" -bool "false" && killall Dock
+
+#   echo "Changing screenshot image type to JPG"
+#   defaults write com.apple.screencapture type JPG
+
+#   echo "Disabling screenshot shadow"
+#   defaults write com.apple.screencapture "disable-shadow" -bool "true"  
+
+#   echo "Setting screenshot location to /Documents/Screenshots"
+#   defaults write com.apple.screencapture "location" -string "~/Pictures/Screenshots" && killall SystemUIServer
+
+#   echo "Show hidden files enabled"
+#   defaults write com.apple.Finder "AppleShowAllFiles" -bool "true" && killall Finder
+
+#   echo "Show Music song notifications"
+#   defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "true" && killall Music
+#   for number in $(seq ${_start} ${_end})
+#     do
+#     sleep 0.01
+#     ProgressBar ${number} ${_end}
+#     done
+#     printf '\nFinished!\n'
+#     fi
+
+
+  
