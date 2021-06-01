3c39c3477b84f3c8a34dd8540634b3e1627e5d34
Kellan2002 <45543616+Kellan2002@users.noreply.github.com>
2021-06-01 16:35:02 +0200

    Cleaned up code and output format

--- script.sh
+++ script.sh
@@ -16,22 +16,15 @@ printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
 
 }
 
-# Variables
 _start=1
 
-# This accounts as the "totalState" variable for the ProgressBar function
-_end=100
-
-echo "Hello welcome to Kellan's bash script for bootstrapping MacOS"
-#read -n1 -p "Have you installed Apple's command line tools (xcode-select)? Yes[y]/No[n] " xcode_installed
-
-echo 
+printf -- "\033[37m Hello welcome to Kellan's bash script for bootstrapping MacOS \033[0m\n"
 
 flag=true
 
 while true; do  # only breaks out of while loop once user chooses y/Y or n/N
   echo
-  read -n1 -p "Have you installed Apple's command line tools (xcode-select)? Yes[y]/No[n] " xcode_installed
+  read -n1 -p "Have you installed Apple's command line tools (xcode-select)? Yes[y]/No[n]" xcode_installed
 
   case $xcode_installed in  # case statement checking userinput for var $xcode_installed  
     y|Y) 
@@ -40,11 +33,11 @@ while true; do  # only breaks out of while loop once user chooses y/Y or n/N
         break;;
     n|N) 
         echo;
-        echo "Please run 'xcode-select install', then try running this script again";
+        printf -- "\033[31m Please run 'xcode-select install', then try running this script again \033[0m\n";
         exit;;
     *)  
         echo;
-        echo "Please enter 'y' for Yes or 'n' for No";;
+        printf "\033[31m Please enter 'y' for Yes or 'n' for No \033[0m\n";;
     esac
 done
 
@@ -62,42 +55,90 @@ while true; do
   esac
 done
 
+while true; do
+  echo
+  read -n1 -p "Is Homebrew already installed? Yes[y]/No[n] " brew_check
+
+  case $brew_check in
+    y|Y)
+      cbrewflag=true;
+      break;;
+    n|N)
+      cbrewflag=false;
+      break;;
+  esac
+done
+
+if $brew_check -eq false; then
+  while true; do
+  echo
+  read -n1 -p "Would you like to install homebrew? Yes[y]/No[n]" brew_install
+  #read -n1 -p "Would you like to install Homebrew packages? Yes[y]/No[n] " brew_file
+
+  case $brew_install in
+    y|Y)
+      brewinsallflag=true;
+      break;;
+    n|N)
+      brewinsallflag=false;
+      break;;
+  esac
+done
+fi
+
+if $brewinsallflag -eq true; then
+  printf -- '\033[33m You will need to input root password for installing Homebrew \033[0m\n';
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+fi
+
+if $brewfileflag -eq true; then
+  brew bundle --file Brewfile
+fi
+
 echo
-echo 
 
-if $flag -eq true; then
-echo -ne '##                        (5%)\r'
+while true; do
+  echo
+  read -n1 -p "Would you like to change prefrences? Yes[y]/No[n] " pref_ans
+
+  case $pref_ans in
+    y|Y)
+      _pref=true;
+      break;;
+    n|N)
+      _pref=false;
+      break;;
+  esac
+done
+
+if $_pref -eq true; then
 defaults write com.apple.dock "orientation" -string "left" && killall Dock;
-sleep 0.1
-echo -ne '####                      (15%)\r'
+printf -- "\e[2m - Process (1/12)\n";
 defaults write com.apple.dock "autohide" -bool "true" && killall Dock;
-sleep 0.1
-echo -ne '#######                   (33%)\r'
+printf -- "\e[2m - Process (2/12)\n";
 defaults write com.apple.dock autohide-time-modifier -float 0.125 && killall Dock;
-sleep 0.9
+printf -- "\e[2m - Process (3/12)\n";
 defaults write com.apple.dock "show-recents" -bool "false" && killall Dock;
-sleep 0.4
-echo -ne '############              (55%)\r'
+printf -- "\e[2m - Process (4/12)\n";
 defaults write com.apple.screencapture type JPG;
-sleep 0.1
-echo -ne '##############            (66%)\r'
+printf -- "\e[2m - Process (5/12)\n";
 defaults write com.apple.screencapture "disable-shadow" -bool "true" ;
-sleep 0.8
-echo -ne '################          (77%)\r'
+printf -- "\e[2m - Process (6/12)\n";
 defaults write com.apple.screencapture "location" -string "~/Pictures/Screenshots" && killall SystemUIServer;
-sleep 0.5
-echo -ne '##################        (88%)\r'
+printf -- "\e[2m - Process (7/12)\n";
 defaults write com.apple.Finder "AppleShowAllFiles" -bool "true" && killall Finder;
-sleep 0.1
-echo -ne '#####################     (95%)\r'
-defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "true" && killall Music;
+printf -- "\e[2m - Process (8/12)\n";
+defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "true";
+printf -- "\e[2m - Process (9/12)\n";
 defaults write -g ApplePressAndHoldEnabled -bool false;
+printf -- "\e[2m - Process (10/12)\n";
 defaults write com.apple.dock autohide-time-modifier -float 0; killall Dock ;
+printf -- "\e[2m - Process (11/12)\n";
 defaults write com.apple.dock autohide-delay -float 0; killall Dock;
-sleep 0.2
-echo -ne '######################### (100%)\r'
+printf -- "\e[2m - Process (12/12)\n";
 
-sleep 0.1
-echo -ne '\n'
-echo 'Finished!'
+printf -- '\n';
+printf -- '\033[32m Finished!'
+exit 0;
 fi
