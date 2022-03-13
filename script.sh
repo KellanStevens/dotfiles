393778bee21aeb3104209c4f5ab1353c3bb91b56
Kellan Stevens <kellanstevens@Kellans-MacBook-Pro.local>
2022-03-13 15:28:42 +0200

    Added ALOT, refactoring, removed redundent user interaction

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 1.1 Input is currentState($1) and totalState($2)
+# Input is currentState($1) and totalState($2)
 function ProgressBar {
 # Process data
     let _progress=(${1}*100/${2}*100)/100
@@ -9,9 +9,9 @@ function ProgressBar {
     _fill=$(printf "%${_done}s")
     _empty=$(printf "%${_left}s")
 
-# 1.2 Build progressbar strings and print the ProgressBar line
-# 1.2.1 Output example:                           
-# 1.2.1.1 Progress : [########################################] 100%
+# Build progressbar strings and print the ProgressBar line
+# Output example:                           
+# Progress : [########################################] 100%
 printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
 
 }
@@ -22,113 +22,170 @@ printf -- "\033[37m Hello welcome to Kellan's bash script for bootstrapping MacO
 
 flag=true
 
-while true; do  # only breaks out of while loop once user chooses y/Y or n/N
-  echo
-  read -n1 -p "Have you installed Apple's command line tools (xcode-select)? Yes[y]/No[n]" xcode_installed
 
-  case $xcode_installed in  # case statement checking userinput for var $xcode_installed  
-    y|Y) 
-        echo ;
-        echo "You can go forward";
-        break;;
-    n|N) 
-        echo;
-        printf -- "\033[31m Please run 'xcode-select install', then try running this script again \033[0m\n";
-        exit;;
-    *)  
-        echo;
-        printf "\033[31m Please enter 'y' for Yes or 'n' for No \033[0m\n";;
+xcode-select -p 1>/dev/null;xcode_installed=$?
+if [ $xcode_installed -ne 0 ]; then
+  while true; do
+  read -n1 -p "Apple's command line tools (xcode-select) is not installed, would you like to install it? Yes[y]/No[n] " xcode_install
+    case $xcode_install in  # case statement checking userinput for var $xcode_installed  
+      y|Y) 
+          echo ;
+          echo "You can go forward";
+          break;;
+      n|N) 
+          echo;
+          printf -- "\033[31m Please run 'xcode-select install', then try running this script again \033[0m\n";
+          exit;;
+      *)  
+          echo;
+          printf "\033[31m Please enter 'y' for Yes or 'n' for No \033[0m\n";;
     esac
-done
+  done
+fi
 
-while true; do
+# while true; do  # Only breaks out of while loop once user chooses y/Y or n/N
+#   echo
+#   read -n1 -p "Have you installed Apple's command line tools (xcode-select)? Yes[y]/No[n]" xcode_installed
+
+#   case $xcode_installed in  # case statement checking userinput for var $xcode_installed  
+#     y|Y) 
+#         echo ;
+#         echo "You can go forward";
+#         break;;
+#     n|N) 
+#         echo;
+#         printf -- "\033[31m Please run 'xcode-select install', then try running this script again \033[0m\n";
+#         exit;;
+#     *)  
+#         echo;
+#         printf "\033[31m Please enter 'y' for Yes or 'n' for No \033[0m\n";;
+#     esac
+# done
+
+while true; do # Only breaks out of while loop once user chooses y/Y or n/N
   echo
   read -n1 -p "Would you like to use this repo's zsh config? Yes[y]/No[n] " zshrc_config
-
+  echo
   case $zshrc_config in
     y|Y)
+      cp zshrc ~/.zshrc
       flag=true;
+      echo "zsh conf set"
+      if [ -d ~/.oh-my-zsh ]; then
+        echo "oh-my-zsh already installed"
+      else
+        echo "oh-my-zsh is not installed, busy installing"
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        echo "oh-my-zsh setup"
+      fi
       break;;
     n|N)
       flag=false;
       break;;
+    *)  
+      echo;
+      printf "\033[31m Please enter 'y' for Yes or 'n' for No \033[0m\n";;
   esac
 done
 
-while true; do
-  echo
-  read -n1 -p "Is Homebrew already installed? Yes[y]/No[n] " brew_check
-
-  case $brew_check in
-    y|Y)
-      cbrewflag=true;
-      break;;
-    n|N)
-      cbrewflag=false;
-      break;;
-  esac
-done
+# Checks if Homebrew is installed
+which -s brew
+if [[ $? != 0 ]] ; then
+    brew_check=true
+else
+    brew_check=false
+fi
 
 if $brew_check -eq false; then
-  while true; do
+  while true; do # Only breaks out of while loop once user chooses y/Y or n/N
   echo
   read -n1 -p "Would you like to install homebrew? Yes[y]/No[n]" brew_install
 
   case $brew_install in
     y|Y)
-      brewinsallflag=true;
+      # Installs Homebrew
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       break;;
+
     n|N)
-      brewinsallflag=false;
       break;;
+
+    *)  
+      echo;
+      printf "\033[31m Please enter 'y' for Yes or 'n' for No \033[0m\n";;
   esac
 done
 fi
 
-if $brewinsallflag -eq true; then
-  printf -- '\033[33m You will need to input root password for installing Homebrew \033[0m\n';
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+while true; do # Only breaks out of while loop once user chooses y/Y or n/N
+  echo
+  read -n1 -p "Would you like to install my brew package list? Yes[y]/No[n]" brewfileflag
 
-fi
+  case $brewfileflag in
+    y|Y)
+      brewpackages=true
+      break;;
 
-if $brewfileflag -eq true; then
-  brew bundle --file Brewfile
+    n|N)  
+      brewpackages=false
+      break;;
+
+    *)  
+      echo;
+      printf "\033[31m Please enter 'y' for Yes or 'n' for No \033[0m\n";;
+  esac
+done
+
+if $brewpackages -eq true; then
+while true; do # Only breaks out of while loop once user chooses 1, 2 or 3
+      echo
+      read -n1 -p "Would you like to install full list of packages, minimal packages, or development only packages? 1/2/3 " brewfiletype
+      echo
+
+      case $brewfiletype in 
+      1)
+        brew bundle --file BrewfileFull
+        break;;
+
+      2)
+        brew bundle --file BrewfileMin
+        break;;
+
+      3)
+        brew bundle --file BrewfileDev
+        break;;
+      
+      *)  
+        echo;
+        printf "\033[31m Please enter 1, 2 or 3 \033[0m\n";;
+      esac
+    done
 fi
 
 echo
 
-while true; do
-  echo
-  read -n1 -p "Would you like to setup fish as your default shell? Yes[y]/No[n]" fish_set
 
-  case $fish_set in 
-    y|Y)
-      echo "Setting FISH as default shell" ;
-      chsh -s /usr/local/bin/fish ;
-      echo function fish_greeting\n\tneofetch\nend >> ~/.config/fish/config.fish ;
-      curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish ;
-      omf install robbyrussell
-    n|N)
-      break;;
-    esac
-done
-
-while true; do
+while true; do # Only breaks out of while loop once user chooses y/Y or n/N
   echo
-  read -n1 -p "Would you like to change prefrences? Yes[y]/No[n] " pref_ans
+  read -n1 -p "Would you like to set my prefrences? Yes[y]/No[n] " pref_ans
 
   case $pref_ans in
     y|Y)
       _pref=true;
       break;;
+      
     n|N)
       _pref=false;
       break;;
+    *)  
+      echo;
+      printf "\033[31m Please enter 'y' for Yes or 'n' for No \033[0m\n";;
   esac
 done
 
 if $_pref -eq true; then
-defaults write com.apple.dock "orientation" -string "left" && killall Dock;
+defaults write com.apple.dock "orientation" -string "bottom" && killall Dock;
 printf -- "\e[2m - Process (1/12)\n";
 defaults write com.apple.dock "autohide" -bool "true" && killall Dock;
 printf -- "\e[2m - Process (2/12)\n";
