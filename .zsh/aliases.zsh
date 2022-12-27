f29add6ea735fde0d72874064c5924ba2f14bf1c
Kellan Stevens <kellan@kellanstevens.com>
2022-12-27 10:55:39 +0200

    Added apparency to BrewFile and added to aliases config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,5 @@
 alias ls="exa --icons --group-directories-first"
 alias ll="exa --icons --group-directories-first -l"
+alias la="exa --icons --group-directories-first -la"
+
 alias grep='grep --color'
