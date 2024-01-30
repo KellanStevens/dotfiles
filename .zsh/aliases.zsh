314038a4fb46181bb19a587638d197d87a228d1a
Kellan Stevens <kellan@kellanstevens.com>
2024-01-30 09:59:42 +0200

    Updating aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,5 +1,19 @@
-alias ls="exa --icons --group-directories-first"
-alias ll="exa --icons --group-directories-first -l"
-alias la="exa --icons --group-directories-first -la"
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
+alias la="eza --icons --group-directories-first -la"
 
 alias grep='grep --color'
+
+alias killphp='lsof -i:3000 | awk "NR!=1 {print $2}" | xargs kill -9'
+
+alias brwup='brew upgrade -v && brew upgrade -v -g'
+
+alias kas-dev='ssh kas-dev.t100.uk'
+alias blm-dev='ssh blm-dev.t100.uk'
+
+alias apits-test='ssh apits-test.t100.uk'
+alias apits-live='ssh apits-live.t100.uk'
+
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+
+alias pa='php artisan'
