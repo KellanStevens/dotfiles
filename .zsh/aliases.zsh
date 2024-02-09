8ab4896f332ae01a72d97d4a3c3b8f9e679a244b
Kellan Stevens <kellan@kellanstevens.com>
2024-02-09 14:22:21 +0200

    Update aliases with pest command and removing old SSH aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -8,12 +8,8 @@ alias killphp='lsof -i:3000 | awk "NR!=1 {print $2}" | xargs kill -9'
 
 alias brwup='brew upgrade -v && brew upgrade -v -g'
 
-alias kas-dev='ssh kas-dev.t100.uk'
-alias blm-dev='ssh blm-dev.t100.uk'
-
-alias apits-test='ssh apits-test.t100.uk'
-alias apits-live='ssh apits-live.t100.uk'
-
 alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+
+alias pest='./vendor/bin/pest'
 
 alias pa='php artisan'
