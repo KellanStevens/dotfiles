1c00f64ef8f970129842f36b70eb0d70ef6bce53
Kellan Stevens <kellan@kellanstevens.com>
2024-12-23 00:19:02 +0200

    chore(config/vscode/extensions.sh): cleanup and add unadded extensions

---
+++ .config/vscode/extensions.sh
@@ -0,0 +1,60 @@
+code --list-extensions | xargs -L 1 echo code --install-extension
+
+# Looks
+code --install-extension catppuccin.catppuccin-vsc
+code --install-extension catppuccin.catppuccin-vsc-icons
+code --install-extension equinusocio.moxer-icons
+code --install-extension drcika.apc-extension
+code --install-extension be5invis.vscode-custom-css
+code --install-extension rafamel.subtle-brackets
+
+# Languages
+code --install-extension hashicorp.hcl
+code --install-extension vue.volar
+code --install-extension bbenoist.nix
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension mblode.twig-language-2
+code --install-extension msjsdiag.vscode-react-native
+code --install-extension pranaygp.vscode-css-peek
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension william-voyek.vscode-nginx
+
+# Copilot
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+
+# Microsoft
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode.remote-explorer
+
+# Git
+code --install-extension waderyan.gitblame
+code --install-extension github.vscode-pull-request-github
+code --install-extension mhutchie.git-graph
+code --install-extension eamodio.gitlens
+code --install-extension aaron-bond.better-comments
+code --install-extension alefragnani.project-manager
+code --install-extension donjayamanne.githistory
+code --install-extension codezombiech.gitignore
+code --install-extension git-extension-pack.donjayamanne
+code --install-extension vscode-open-in-github.ziyasal
+
+# Misc
+code --install-extension tomoki1207.pdf
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension formulahendry.code-runner
+code --install-extension vincaslt.highlight-matching-tag
+code --install-extension editorconfig.editorconfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension xdebug.php-debug
+code --install-extension bradzacher.vscode-copy-filename
+code --install-extension formulahendry.auto-complete-tag
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension figma.figma-vscode-extension
