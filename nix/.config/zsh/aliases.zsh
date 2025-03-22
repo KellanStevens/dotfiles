dbe85484cc9ad49aa5c8800e4552eab216046243
Kellan Stevens <kellan@kellanstevens.com>
2025-03-22 15:18:22 +0200

    chore(.config/zsh/aliases.zsh): added new alias

--- nix/.config/zsh/aliases.zsh
+++ nix/.config/zsh/aliases.zsh
@@ -21,3 +21,4 @@ alias android_emu='emulator -avd Pixel_6_Pro_API_35_rootable_ -writable-system -
 alias rebuild-nix='darwin-rebuild switch --flake ~/.config/nix'
 
 alias lgit='lazygit'
+alias pa='php artisan' 
