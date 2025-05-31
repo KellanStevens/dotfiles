3c044ec07870b0b75a1c1e15977b95d6aee4cc07
Kellan Stevens <kellan@kellanstevens.com>
2025-05-31 16:43:54 +0200

    chore(nix/.config/zsh/aliases.zsh): update aliases

--- nix/.config/zsh/aliases.zsh
+++ nix/.config/zsh/aliases.zsh
@@ -7,18 +7,11 @@ alias cat='bat'
 
 alias brwup='brew update -v && brew upgrade -v -g'
 
-alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
-alias pest='./vendor/bin/pest'
-alias sniff='vendor/bin/phpcs --standard=PSR12 app'
-alias pa='php artisan'
-
 alias tldu='tl dev up'
-alias tldn='tl dev up --run-prune'
+alias tldn='tl dev up --run'
 
-alias vim='nvim'
 alias android_emu='emulator -avd Pixel_6_Pro_API_35_rootable_ -writable-system -no-snapshot-load'
 
 alias rebuild-nix='darwin-rebuild switch --flake ~/.config/nix'
 
-alias lgit='lazygit'
-alias pa='php artisan' 
+alias pa='php artisan'
